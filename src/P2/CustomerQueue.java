277bc4b0aba12a3677f9c60ad7b182d86bfffb50
DagInge <dihelgoy@gmail.com>
2015-02-23 17:39:48 +0100

    Implemented the CustomerQueue

--- src/P2/CustomerQueue.java
+++ src/P2/CustomerQueue.java
@@ -1,5 +1,6 @@
 package P2;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,23 +13,45 @@ public class CustomerQueue {
 	 * @param queueLength	The maximum length of the queue.
 	 * @param gui			A reference to the GUI interface.
 	 */
-    private List queue;
+    private List<Customer> queue;
     private Gui gui;
     private int queueLength;
+    private ArrayList<Customer> seats;
 
     public CustomerQueue(int queueLength, Gui gui) {
 		this.gui = gui;
         this.queueLength = queueLength;
 
-        queue = new LinkedList<Customer>();
-        addCustomer();
+        queue = new LinkedList();
+        seats = new ArrayList<Customer>();
+        for (int n = 0; n < queueLength; n++){
+            seats.add(null);
+        }
+
+        for(int i = 0; i<7; i++){
+            addCustomer(new Customer());
+        }
 	}
 
-    public void addCustomer(){
-        Customer customer1 = new Customer();
-        gui.fillLoungeChair(0, customer1);
-        queue.add(customer1);
+    public void addCustomer(Customer customer){
+        if (queue.size() < queueLength){
+            queue.add(customer);
+            for(int i = 0; i < queueLength; i++){
+                if (seats.get(i)==null){
+                    seats.set(i, customer);
+                    gui.fillLoungeChair(i, customer);
+                    break;
+                }
+            }
+        }
+    }
 
+    public Customer removeCustomer(){
+        Customer c = queue.remove(0);
+        int index = seats.indexOf(c);
+        seats.set(index, null);
+        gui.emptyLoungeChair(index);
+        return c;
     }
 
 	// Add more methods as needed
