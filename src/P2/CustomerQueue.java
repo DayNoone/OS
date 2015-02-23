c1ce8d708518ffef611ed55bdcf310a50722d586
DagInge <dihelgoy@gmail.com>
2015-02-23 16:54:19 +0100

    First customer

--- src/P2/CustomerQueue.java
+++ src/P2/CustomerQueue.java
@@ -1,4 +1,8 @@
 package P2;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This class implements a queue of customers as a circular buffer.
  */
@@ -8,9 +12,24 @@ public class CustomerQueue {
 	 * @param queueLength	The maximum length of the queue.
 	 * @param gui			A reference to the GUI interface.
 	 */
+    private List queue;
+    private Gui gui;
+    private int queueLength;
+
     public CustomerQueue(int queueLength, Gui gui) {
-		// Incomplete
+		this.gui = gui;
+        this.queueLength = queueLength;
+
+        queue = new LinkedList<Customer>();
+        addCustomer();
 	}
+
+    public void addCustomer(){
+        Customer customer1 = new Customer();
+        gui.fillLoungeChair(0, customer1);
+        queue.add(customer1);
+
+    }
 
 	// Add more methods as needed
 }
